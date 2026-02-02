18d84aaeb71a96e852ddb7f13bd6f2122ea67787
Tkcool28 <toddkirschman@gmail.com>
2026-02-01 21:09:34 -0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "calorie-quest-v4";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./sw.js"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
